455583038c4e3af86f868b841aa68269d399e679
unknown <bhagyashree9402@gmail.com>
2020-08-10 19:38:33 -0700

    Updates

---
+++ precache-manifest.9905313bac0ee1152bb634add04d79eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7636d2914d4fdce6f65b4a3ae7f20008",
+    "url": "/mern_react/index.html"
+  },
+  {
+    "revision": "d05435ed6ce15a9cc0db",
+    "url": "/mern_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/mern_react/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mern_react/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d05435ed6ce15a9cc0db",
+    "url": "/mern_react/static/js/main.e2617e75.chunk.js"
+  },
+  {
+    "revision": "08591786f86c4ac9b0e2",
+    "url": "/mern_react/static/js/runtime-main.4d375656.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mern_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
